12a0bae36e71d26c494cd60de23364c0c67f26af
uustop <yth3412@163.com>
2018-01-23 15:11:42 +0800

    Initial commit

---
+++ eureka-consumer/src/main/java/com/uustop/eurekaconsumer/EurekaConsumerApplication.java
@@ -0,0 +1,15 @@
+package com.uustop.eurekaconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EurekaConsumerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaConsumerApplication.class, args);
+	}
+}
